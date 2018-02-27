70646d2f64c2e6c679006ca8cfb210445018ae0c
leoaw <leoaw@users.noreply.github.com>
2018-02-27 11:09:27 +0700

    Add files via upload

--- assets/js/jQuery.scrollSpeed.js
+++ assets/js/jQuery.scrollSpeed.js
@@ -1,1 +1,98 @@
-!function(t){jQuery.scrollSpeed=function(e,n,i){var o,l,r,u=t(document),h=t(window),a=t("html, body"),c=i||"default",d=0,f=!1;if(window.navigator.msPointerEnabled)return!1;h.on("mousewheel DOMMouseScroll",function(t){var i=t.originalEvent.wheelDeltaY,s=t.originalEvent.detail;return o=u.height()>h.height(),l=u.width()>h.width(),f=!0,o&&(r=h.height(),(i<0||s>0)&&(d=d+r>=u.height()?d:d+=e),(i>0||s<0)&&(d=d<=0?0:d-=e),a.stop().animate({scrollTop:d},n,c,function(){f=!1})),l&&(r=h.width(),(i<0||s>0)&&(d=d+r>=u.width()?d:d+=e),(i>0||s<0)&&(d=d<=0?0:d-=e),a.stop().animate({scrollLeft:d},n,c,function(){f=!1})),!1}).on("scroll",function(){o&&!f&&(d=h.scrollTop()),l&&!f&&(d=h.scrollLeft())}).on("resize",function(){o&&!f&&(r=h.height()),l&&!f&&(r=h.width())})},jQuery.easing.default=function(t,e,n,i,o){return-i*((e=e/o-1)*e*e*e-1)+n}}(jQuery);
+// Plugin: jQuery.scrollSpeed
+// Source: github.com/nathco/jQuery.scrollSpeed
+// Author: Nathan Rutzky
+// Update: 1.0.2
+
+(function($) {
+    
+    jQuery.scrollSpeed = function(step, speed, easing) {
+        
+        var $document = $(document),
+            $window = $(window),
+            $body = $('html, body'),
+            option = easing || 'default',
+            root = 0,
+            scroll = false,
+            scrollY,
+            scrollX,
+            view;
+            
+        if (window.navigator.msPointerEnabled)
+        
+            return false;
+            
+        $window.on('mousewheel DOMMouseScroll', function(e) {
+            
+            var deltaY = e.originalEvent.wheelDeltaY,
+                detail = e.originalEvent.detail;
+                scrollY = $document.height() > $window.height();
+                scrollX = $document.width() > $window.width();
+                scroll = true;
+            
+            if (scrollY) {
+                
+                view = $window.height();
+                    
+                if (deltaY < 0 || detail > 0)
+            
+                    root = (root + view) >= $document.height() ? root : root += step;
+                
+                if (deltaY > 0 || detail < 0)
+            
+                    root = root <= 0 ? 0 : root -= step;
+                
+                $body.stop().animate({
+            
+                    scrollTop: root
+                
+                }, speed, option, function() {
+            
+                    scroll = false;
+                
+                });
+            }
+            
+            if (scrollX) {
+                
+                view = $window.width();
+                    
+                if (deltaY < 0 || detail > 0)
+            
+                    root = (root + view) >= $document.width() ? root : root += step;
+                
+                if (deltaY > 0 || detail < 0)
+            
+                    root = root <= 0 ? 0 : root -= step;
+                
+                $body.stop().animate({
+            
+                    scrollLeft: root
+                
+                }, speed, option, function() {
+            
+                    scroll = false;
+                
+                });
+            }
+            
+            return false;
+            
+        }).on('scroll', function() {
+            
+            if (scrollY && !scroll) root = $window.scrollTop();
+            if (scrollX && !scroll) root = $window.scrollLeft();
+            
+        }).on('resize', function() {
+            
+            if (scrollY && !scroll) view = $window.height();
+            if (scrollX && !scroll) view = $window.width();
+            
+        });       
+    };
+    
+    jQuery.easing.default = function (x,t,b,c,d) {
+    
+        return -c * ((t=t/d-1)*t*t*t - 1) + b;
+    };
+    
+})(jQuery);
