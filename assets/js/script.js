2da58c2cc0edf499ddc333c79acc22b11727263a
leoaw <leoaw@users.noreply.github.com>
2020-01-20 11:46:45 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-$('.box').circursor({
-    size1: "4px",
-    width1: "4px",
-    color1: "rgba(255, 235, 242, 0.9)",
-    duration1: "200",
-    size2: "30px",
-    width2: "30px",
-    color2: "rgba(217, 0, 70, 0.69)",
-    duration2: "2000",
-    easing: "easeOut"
-});
-
 $('html').easeScroll({
     frameRate: 30,
     animationTime: 2000,
